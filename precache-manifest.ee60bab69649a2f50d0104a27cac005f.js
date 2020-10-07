4fbbeaa2a0c20fa6db1842f32faa1cdf1b737cb0
tevtonsky1054m1@gmail.com <tevtonsky1054m1@gmail.com>
2020-10-08 00:08:40 +0300

    Updates

---
+++ precache-manifest.ee60bab69649a2f50d0104a27cac005f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7d311e18752152170d3082655c7fc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5063efb8fc437d61bd99",
+    "url": "/static/css/main.53a72c84.css"
+  },
+  {
+    "revision": "5063efb8fc437d61bd99",
+    "url": "/static/js/main.c8f16e17.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/main.c8f16e17.js.LICENSE.txt"
+  },
+  {
+    "revision": "981535f1134a0b8f718f",
+    "url": "/static/js/vendors~canvg.e561d91b.chunk.js"
+  },
+  {
+    "revision": "6ce3b560233153332a8f",
+    "url": "/static/js/vendors~pdfmake.09e9dd8d.chunk.js"
+  },
+  {
+    "revision": "e3b93feea552d5fd932a1758b1da5f1a",
+    "url": "/static/js/vendors~pdfmake.09e9dd8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdd1d468b90812aed2d3",
+    "url": "/static/js/vendors~xlsx.e43e462d.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/static/js/vendors~xlsx.e43e462d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e086e955eb63e71b5b26",
+    "url": "/static/js/xlsx.af814cf1.chunk.js"
+  }
+]);
